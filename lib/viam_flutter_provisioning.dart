4cf7fbb04007669c0ca3af095b0a96e6d0d6a8ee
Kevin Johnson <kevin.johnson@viam.com>
2025-02-25 15:39:44 -0500

    add viam-sdk as dependency, start filling in + todos

--- lib/viam_flutter_provisioning.dart
+++ lib/viam_flutter_provisioning.dart
@@ -1,9 +1,36 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+import 'dart:async';
 
-class Viamerovisioning {
-  // TODO: implement
+import 'package:viam_sdk/viam_sdk.dart';
+
+class ViamProvisioniong {
+  final ProvisioningClient provisioningClient;
+
+  ViamProvisioniong({required this.provisioningClient});
+  // TODO: list local bluetooth devices (do this w/ flutter_blue and show in example app)
+  // TODO: connect/auto-pair to the bluetooth device
+
+  // TODO: add logic for listing wifi networks* (though currently hotspot connect happens by going to settings)
+
+  /// Gets a list of networks from viam-agent that are visible to the smart machine.
+  Future<List<NetworkInfo>> getNetworkList() async {
+    final response = await provisioningClient.getNetworkList();
+    return response.toList()..sort((b, a) => a.signal.compareTo(b.signal));
+  }
+
+  /// setNetworkCredentials and setSmartMachineCredentials need to be called as close to eachother as possible,
+  /// there is a 10 second window on the viam-agent side once one has been recieved before it turns off the
+  /// hotspot*** (what about bluetooth?) and attempts to connect.
+  ///
+  /// Sends the network credentials for Wi-Fi to the smart machine to add
+  /// to its network manager and attempt to connect. If the network has no passkey,
+  /// send an empty string.
+  Future<void> setNetworkCredentials(String ssid, String psk) async {
+    await provisioningClient.setNetworkCredentials(ssid: ssid, psk: psk);
+  }
+
+  /// Sends the Viam Robot Secret & ID to the smart machine to store in its viam.json
+  Future<void> setSmartMachineCredentials(String id, String secret) async {
+    // TODO: need a mainPart for this id and secret! we create a new robot when we do this currently
+    await provisioningClient.setSmartMachineCredentials(id: id, secret: secret);
+  }
 }
--- test/viam_flutter_provisioning_test.dart
+++ test/viam_flutter_provisioning_test.dart
@@ -3,10 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:viam_flutter_provisioning/viam_flutter_provisioning.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('', () {
+    // TODO: implement
   });
 }
